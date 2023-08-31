ee8dc70cde60a6898877b0bf8c0846f062bea4f8
menezesledilson <ledilsonmenezes@gmail.com>
2023-08-31 16:27:35 -0300

    aula 62

---
+++ Java/Exemplos/src/javaCore/Fmodificadorestatico/domain/Carro.java
@@ -0,0 +1,47 @@
+package javaCore.Fmodificadorestatico.domain;
+
+public class Carro {
+
+    private String nome;
+    private double velocidadeMaxima;
+    private static double velocidadeLimite = 250; //modificado estatico
+
+    public Carro(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+
+    }
+
+    public void imprime() {
+        System.out.println("---------------------");
+        System.out.println("nome " + this.nome);
+        System.out.println("Velocidade Maxima " + this.velocidadeMaxima);
+        System.out.println("Velocidade Limite " + Carro.velocidadeLimite);
+
+    }
+
+    public static void setVelocidadeLimite(double velocidadeLimite) {
+        Carro.velocidadeLimite = velocidadeLimite;
+    }
+    public  static double getVelocidadeLimite(){
+        return Carro.velocidadeLimite;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+}
