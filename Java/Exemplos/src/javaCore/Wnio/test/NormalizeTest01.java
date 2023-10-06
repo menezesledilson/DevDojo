d67064ebd13b6bc99e01b609adf3582aa80cbdac
menezesledilson <ledilsonmenezes@gmail.com>
2023-10-06 18:32:54 -0300

    normalize

---
+++ Java/Exemplos/src/javaCore/Wnio/test/NormalizeTest01.java
@@ -0,0 +1,20 @@
+package javaCore.Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NormalizeTest01 {
+    public static void main(String[] args) {
+        String diretorioProjeto = "home/william/dev";
+        String arquivotTXT = "../../arquivo.txt";
+        Path path1 = Paths.get(diretorioProjeto, arquivotTXT);
+        System.out.println(path1);
+        System.out.println(path1.normalize());
+
+        Path path2 = Paths.get("home/./william/./dev/");
+
+        System.out.println(path2);
+        System.out.println(path2.normalize());
+
+    }
+}
