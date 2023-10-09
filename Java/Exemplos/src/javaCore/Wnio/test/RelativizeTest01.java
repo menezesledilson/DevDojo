681c7148b5a3dc95a415e86e23aa95579de97062
menezesledilson <ledilsonmenezes@gmail.com>
2023-10-09 10:12:36 -0300

    NormalizeTest,ResolveTest

---
+++ Java/Exemplos/src/javaCore/Wnio/test/RelativizeTest01.java
@@ -0,0 +1,26 @@
+package javaCore.Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class RelativizeTest01 {
+
+    public static void main(String[] args) {
+        Path dir = Paths.get("/home/William");
+        Path clazz = Paths.get("/home/William/devdojofoda/OlaMundo.java");
+        Path pathToClazz = dir.relativize(clazz);
+        System.out.println(pathToClazz);
+
+        Path absoluto1 = Paths.get("/home/william");
+        Path absoluto2 = Paths.get("/usr/local");
+        Path absoluto3 = Paths.get("/home/William/devdojofoda/OlaMundo.java");
+        Path relativo1 = Paths.get("temp");
+        Path relativo2 = Paths.get("temp/temp.2021921");
+
+        System.out.println("1 " +absoluto1.relativize(absoluto3));
+        System.out.println("2 " +absoluto3.relativize(absoluto1));
+        System.out.println("3 " +absoluto1.relativize(absoluto2));
+        System.out.println("4 " +absoluto1.relativize(relativo2));
+      //  System.out.println("5 " +absoluto1.relativize(absoluto1)); O java não sabe como chegar
+    }
+}
